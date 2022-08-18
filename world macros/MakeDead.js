ade9cc7e58917eac6fcde1b6a892a7e01e53329e
jbowensii <jbowens@aol.com>
2022-08-18 19:03:58 -0400

    last update before V10 compatibility

--- world macros/MakeDead.js
+++
@@ -1,5 +0,0 @@
-let targetUuid = args[0];
-console.log("MACRO TEST | MAKE DEAD");
-const target = (await fromUuid(targetUuid));
-console.log("MACRO TEST | target: %O", target);
-await target.actor.update({ "data.attributes.hp.value": 0 });
